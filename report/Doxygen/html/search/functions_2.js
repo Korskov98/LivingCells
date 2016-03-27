042b6a28b20e7d04ab0bdc12f4d4c9ba8b7db536
user <user@debian>
2016-03-28 00:58:57 +0300

    delete

--- report/Doxygen/html/search/functions_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['get_5fstatus',['get_status',['../classCell.html#a5e113f816cd4750eccd1846af81845ac',1,'Cell']]],
-  ['get_5fx',['get_x',['../classCell.html#a00989b383714d8f6d051677b96a3adcb',1,'Cell::get_x()'],['../classField.html#a87883594a355f7f8d5855a75a9c244ba',1,'Field::get_x()']]],
-  ['get_5fy',['get_y',['../classCell.html#a957598cfc79e75ae651126173425d3fc',1,'Cell::get_y()'],['../classField.html#a88d72b613ec5a84ed771b30c9332a2ee',1,'Field::get_y()']]]
-];
