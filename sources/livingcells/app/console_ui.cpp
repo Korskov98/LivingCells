56357b5aa2df33f8849ee4f16729e5132ad0b8f7
user <user@debian>
2016-04-11 16:16:28 +0300

    исправлеия

--- sources/livingcells/app/console_ui.cpp
+++ sources/livingcells/app/console_ui.cpp
@@ -3,36 +3,36 @@
 
 void console_ui::print_menu()
 {
-    std::cout << "1. Создать новую модель." << std::endl;
-    std::cout << "2. Загрузить модель." << std::endl;
-    std::cout << "0. Выход." << std::endl;
-    int choice;
+    int choice = -1;
     Api a;
     Field f;
     while(choice != 0) {
-            std::cin >> choice;
-            switch (choice)
-            {
-                case 1:
-                    std::cout << "Введите размер поля." << std::endl;
-                    int x,y;
-                    std::cin >> x >> y;
-                    a.initialize_settings(f,x,y);
-                    a.initialize_field(f);
-                    a.print_field(f);
-                    this->secondary_menu(f,a);
-                    break;
-            }
+        std::cout << "1. Создать новую модель." << std::endl;
+        std::cout << "2. Загрузить модель." << std::endl;
+        std::cout << "0. Выход." << std::endl;
+        std::cin >> choice;
+        switch (choice)
+        {
+            case 1:
+                std::cout << "Введите размер поля." << std::endl;
+                int x,y;
+                std::cin >> x >> y;
+                a.initialize_settings(f,x,y);
+                a.initialize_field(f);
+                a.print_field(f);
+                this->secondary_menu(f,a);
+                break;
+        }
     }
 }
 
 void console_ui::secondary_menu(Field &f, Api a)
 {
-    std::cout << "1. Следующее поколение." << std::endl;
-    std::cout << "2. Сохранить модель." << std::endl;
-    std::cout << "0. Назад в главное меню." << std::endl;
-    int choice;
+    int choice = -1;
     while (choice != 0) {
+        std::cout << "1. Следующее поколение." << std::endl;
+        std::cout << "2. Сохранить модель." << std::endl;
+        std::cout << "0. Назад в главное меню." << std::endl;
         std::cin >> choice;
         switch (choice) {
         case 1:
